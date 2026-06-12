203b4528e8eb7116ba2a6ffc9739ed6da5684098
silvanamateus2008-ui <silvanamateus2008@gmail.com>
2026-06-12 16:13:49 -0500

    cambio conexiom

--- src/modelo/ConexionBD.java
+++ src/modelo/ConexionBD.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Singleton.java to edit this template
  */
-package controlador;
+package modelo;
 
 import java.io.InputStream;
 import java.sql.Connection;
